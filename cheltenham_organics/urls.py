1b0f751dc987091e84a9f8de836698c0926a99a6
arthurvguide <arthurvguideuk@gmail.com>
2022-05-11 15:23:31 +0000

    Setup settings and urls to get static files

--- cheltenham_organics/urls.py
+++ cheltenham_organics/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls'))
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
