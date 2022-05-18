679ed05acd56df1da7b9ffbd8d44ca215be32430
arthurvguide <arthurvguideuk@gmail.com>
2022-05-18 14:36:20 +0000

    Initial header structure and style

---
+++ static/js/base.js
@@ -0,0 +1,22 @@
+// Script to make the sidebar visible on mobile devices
+
+$(document).ready(function() {
+    $("#sidebarCollapse").on("click", function() {
+      $("#sidebar").addClass("active");
+    });
+  
+    $("#sidebarCollapseX").on("click", function() {
+      $("#sidebar").removeClass("active");
+    });
+  
+    $("#sidebarCollapse").on("click", function() {
+      if ($("#sidebar").hasClass("active")) {
+        $(".overlay").addClass("visible");
+      }
+    });
+  
+    $("#sidebarCollapseX").on("click", function() {
+      $(".overlay").removeClass("visible");
+    });
+  });
+  
