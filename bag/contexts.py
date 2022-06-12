91d47e3642fec5814f23d811e64365365ef1452f
arthurvguide <arthurvguideuk@gmail.com>
2022-06-12 00:25:22 +0000

    Context bag_contetents access add_to_bag view

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
@@ -7,9 +9,20 @@ def bag_contents(request):
     total = 0
     product_count = 0
     delivery = 2
+    bag = request.session.get('bag', {})
     
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     final_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
