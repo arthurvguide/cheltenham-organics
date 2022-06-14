17382f098fc2a1e7127623ae11254ea51fa19903
arthurvguide <arthurvguideuk@gmail.com>
2022-06-14 13:29:27 +0000

    Install crispy forms and setup

--- cheltenham_organics/settings.py
+++ cheltenham_organics/settings.py
@@ -44,7 +44,10 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
-    'checkout'
+    'checkout',
+
+    # External 
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +61,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'cheltenham_organics.urls'
+
+CRISPY_TEMPLATE_PACK =  'bootstrap4'
 
 TEMPLATES = [
     {
@@ -75,6 +80,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents'
             ],
+            'builtins': [
+                'crispy_forms.templatetag.crispy_forms_tag',
+                'crispy_forms.templatetag.crispy_forms_field',
+            ]
         },
     },
 ]
