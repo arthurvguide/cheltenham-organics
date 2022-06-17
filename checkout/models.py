2d4480bcb91a1b272304eb092b974d719f5d53aa
arthurvguide <arthurvguideuk@gmail.com>
2022-06-17 22:28:02 +0000

    Create UserProfile model and include in the Order Model

--- checkout/models.py
+++ checkout/models.py
@@ -6,10 +6,13 @@ from django.conf import settings
 
 from django_countries.fields import CountryField
 
+from profiles.models import UserProfile
 from products.models import Product
 
 
 class Order(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
