a2da7c4fcea9133b6c04b7d06ac2570103f5ceec
arthurvguide <arthurvguideuk@gmail.com>
2022-06-17 21:47:14 +0000

    Ammend Checkout model and migrate

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
+from django_countries.fields import CountryField
+
 from products.models import Product
 
 
@@ -12,7 +14,7 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -25,6 +27,8 @@ class Order(models.Model):
                                       decimal_places=2, null=False, default=0)
     final_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
