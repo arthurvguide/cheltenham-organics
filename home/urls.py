784be248e462d96efc88f278502eb069a59a8ba4
arthurvguide <arthurvguideuk@gmail.com>
2022-06-23 00:14:05 +0000

    Add Privacy Police and Terms of Use in the footer

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_home, name='home')
+    path('', views.get_home, name='home'),
+    path('privacy-notice', views.privacy_notice, name='privacy_notice'),
+    path('terms_of_use', views.terms_of_use, name='terms_of_use'),
 ]
+
--- home/views.py
+++ home/views.py
@@ -4,3 +4,15 @@ def get_home(request):
     """ A view to return the home page """
     
     return render(request, 'home/index.html')
+
+
+def privacy_notice(request):
+    """ A view to return the privacy_notice """
+    
+    return render(request, 'home/privacy-notice.html')
+
+
+def terms_of_use(request):
+    """ A view to return the terms of use page """
+    
+    return render(request, 'home/terms-of-use.html')
