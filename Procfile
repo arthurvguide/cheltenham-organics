3ced24ad2e99c08d20d002e45332ed35813e9760
arthurvguide <arthurvguideuk@gmail.com>
2022-06-26 20:01:16 +0000

    Initial setup for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cheltenham-organics.wgsi:application
--- cheltenham_organics/settings.py
+++ cheltenham_organics/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yem75wjf9km32gnf5&v@jhw-&@5f+d496+g55ps1fmi1k)w=g*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cheltenham-organics.herokuapp.com/', 'localhost']
 
 
 # Application definition
@@ -107,12 +107,17 @@ WSGI_APPLICATION = 'cheltenham_organics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
