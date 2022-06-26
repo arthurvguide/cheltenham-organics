59e576a2609245111653f7e3148ff0ff07b85ef0
arthurvguide <arthurvguideuk@gmail.com>
2022-06-26 22:36:23 +0000

    Add cache control

--- cheltenham_organics/settings.py
+++ cheltenham_organics/settings.py
@@ -177,6 +177,12 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'cheltenham-organics'
     AWS_S3_REGION_NAME = 'eu-west-2'
