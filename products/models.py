43a0bcc804d431f1be7e1b99d53e95bc8a2c1558
arthurvguide <arthurvguideuk@gmail.com>
2022-06-28 13:59:14 +0000

    Add review model and migrations

--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
+# - - - - - Django Imports - - - - - - - - -
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -13,7 +15,8 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
@@ -22,6 +25,24 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
-
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    """
+    Model for loged-in customers review the products
+    """
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=25,
+                             null=False, blank=False)
+    review_subject = models.TextField(max_length=250, null=False,
+                                      blank=False)
+    date_created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+
+        return self.title
