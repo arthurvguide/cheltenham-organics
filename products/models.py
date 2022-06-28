6d41b65c6af0197ed5cb47b2448cac86b64153fe
arthurvguide <arthurvguideuk@gmail.com>
2022-06-28 09:26:40 +0000

    Remove rating attirube from products

--- products/models.py
+++ products/models.py
@@ -19,7 +19,6 @@ class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
